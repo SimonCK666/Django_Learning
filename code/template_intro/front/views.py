b0280f40f7c3e3f2e665f1102f2a38b42b0767d3
SimonCK666 <SimonYang223@163.com>
2020-09-15 21:06:25 +0800

    'templates'

--- code/template_intro/front/views.py
+++ code/template_intro/front/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 from django.template.loader import render_to_string
 
 def index(request):
-    html = render_to_string("")
+    return render_to_string(request, "front/index.html")
+    # return HttpResponse(html)
